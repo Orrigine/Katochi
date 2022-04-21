629db6dd7e76f009c6b981f1d0c1fb291e8133b7
SmartNewt <mateescuraphael@icloud.com>
2022-04-21 16:11:45 +0200

    ajout page article

--- katochi/src/components/ArticleCard.jsx
+++ katochi/src/components/ArticleCard.jsx
@@ -4,14 +4,13 @@ import { Container, Row, Col } from "react-bootstrap"
 import '../css/App.css';
 import '../css/ArticleCard.css';
 import { Component } from "react";
-import twitch1 from "../img/imgtwitch1.JPG"
 
 class ArticleCard extends Component {
     render() {
         return(
             <Col className="Card">
                 
-                    <img className="img-articleCard" src={twitch1} alt="placeholder" /> 
+                    <img className="img-articleCard" src="" alt="placeholder" /> 
                     <h2>title article Card</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                         ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -21,7 +20,7 @@ class ArticleCard extends Component {
                         sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     
                     <div className="button-div">
-                        <button type="button" className="card-button">Lire l'article</button>
+                        <button type="button" className="card-button" to><Link to={"/articles"}>Lire l'article</Link></button>
                     </div>
             </Col>
         )
--- katochi/src/pages/Articles.jsx
+++ katochi/src/pages/Articles.jsx
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import '../fonts/AdigianaUI.ttf';
-import '../css/accueil.css'
+import '../css/articles.css'
 import { Link } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import { Col, Row, Container } from "react-bootstrap"
 
 class Articles extends Component {
 
@@ -18,11 +18,50 @@ class Articles extends Component {
 render() {
   return (<>
     <Navigation getUserData={this.props.getUserData}/>
+    <div className="article">
+      <Row>
+        <Col lg={4} md={4} xs={4}>
+          <img className="img-articleCard" src="" alt="placeholder" /> 
+        </Col>
+        <Col lg={8} md={8} xs={8}>
+            <h2 className="article-title">title article Card</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </Col>
+      </Row>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+      Excepteur sint occaecat cupidatat non proident,
+      sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+      ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+      Excepteur sint occaecat cupidatat non proident,
+      sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-    <Container className='centered'>
-      <Link to='/'>Revenir sur la page d'accueil</Link>
-    </Container>
-
+      <Row>
+        <Col lg={4} md={4} xs={4}>
+          <p>ecrit par quelqu'un</p>
+        </Col>
+        <Col lg={4} md={4} xs={4}>
+        <div className="button-div">
+            <button type="button" className="card-button" to><Link to={"/articles"}>Revenir sur la page d'accueil</Link></button>
+        </div>
+        </Col>
+          
+        <Col lg={4} md={4} xs={4}>
+          <p className="post-date">date de parution</p>
+        </Col>
+      </Row>
+    </div>
     <Footer />
   </>);
 }
